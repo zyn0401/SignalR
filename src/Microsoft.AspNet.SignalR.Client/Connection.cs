4d7a88f760083f5ab2a0b57949e7ba07024da179
NTaylorMullen <NTaylorMullen@outlook.com>
2013-03-06 23:56:45 -0800

    Added Negotiated event to the C# client
    - Made the negotiated event take in the raw response string.  By default
    the client deserializes the negotiate settings into a
    NegotiationResponse, however if the user modifies the negotiate response
    they may want to deserialize it into their own type of response.
    
    #1638

--- src/Microsoft.AspNet.SignalR.Client/Connection.cs
+++ src/Microsoft.AspNet.SignalR.Client/Connection.cs
@@ -91,6 +91,11 @@ namespace Microsoft.AspNet.SignalR.Client
         public event Action Closed;
 
         /// <summary>
+        /// Occurs when the <see cref="Connection"/> has negotiated.
+        /// </summary>
+        public event Action<string> Negotiated;
+
+        /// <summary>
         /// Occurs when the <see cref="Connection"/> starts reconnecting after an error.
         /// </summary>
         public event Action Reconnecting;
@@ -676,6 +681,15 @@ namespace Microsoft.AspNet.SignalR.Client
             if (Error != null)
             {
                 Error(error);
+            }
+        }
+
+        // We pass a string into OnNegotiated so that users can deserialize the raw data into what they want
+        void IConnection.OnNegotiated(string rawSettings)
+        {
+            if (Negotiated != null)
+            {
+                Negotiated(rawSettings);
             }
         }
 
